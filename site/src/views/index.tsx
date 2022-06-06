b2d68db25c4845b5c8dc2afe1b378bdb9daaa210
hexinmiao <hexinmiao96@126.com>
2022-06-06 15:12:12 +0800

    更改主题样式

--- site/src/views/index.tsx
+++ site/src/views/index.tsx
@@ -9,11 +9,11 @@ export default defineComponent({
   setup() {
     return () => (
       <div style={{ backgroundColor: "#fff", height: "100%" }}>
-        {/* <buttonDemo></buttonDemo>
+        <buttonDemo></buttonDemo>
         <menuDemo></menuDemo>
-        <formDemo></formDemo> */}
+        <formDemo></formDemo>
         {/* <videoDemo></videoDemo> */}
-        <baseDemo></baseDemo>
+        {/* <baseDemo></baseDemo> */}
       </div>
     );
   },
