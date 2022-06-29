f640e44e86145ccd9f09077dee79e814cf70736d
王亮 <674237232@qq.com>
2022-06-29 09:36:02 +0800

    修复 新增弹窗resetFields

--- packages/pc/src/pageComponent/hooks/manage-module/useModalForm.ts
+++ packages/pc/src/pageComponent/hooks/manage-module/useModalForm.ts
@@ -6,14 +6,14 @@
  * @LastEditTime: 2022-04-02 09:53:00
  */
 
-import { ref, watch, nextTick, Ref } from 'vue';
-import { cloneDeep } from 'lodash';
+import { ref, watch, nextTick, Ref } from "vue";
+import { cloneDeep } from "lodash";
 
 export default function useModalForm(
   isVisible: Ref<boolean>,
   getRecord: () => any,
   mode: string,
-  transformForm?: (r: any) => any,
+  transformForm?: (r: any) => any
 ) {
   const formRef = ref();
   const form = ref<any>({});
@@ -22,7 +22,7 @@ export default function useModalForm(
     await nextTick();
     // 打开对话框时复制表单 新建模式不用
     if (val) {
-      if (mode === 'add') return;
+      if (mode === "add") return;
 
       const cloneRecord = cloneDeep(getRecord());
       transformForm && transformForm(cloneRecord);
@@ -30,6 +30,13 @@ export default function useModalForm(
     } else {
       // 清空表单
       formRef.value?.resetFields();
+      // 对Array单独处理 新增模式下属性的初始值是undefined，
+      // resetFields函数使用空数组concat初始值 得到[undifined]
+      for (const key in form.value) {
+        if (Array.isArray(form.value[key])) {
+          form.value[key] = [];
+        }
+      }
     }
   });
 
