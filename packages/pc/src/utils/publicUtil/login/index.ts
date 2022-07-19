12345b34b9f6d65ab4b75cc181cfc8b2e8dcae27
hexinmiao <hexinmiao96@126.com>
2022-07-19 16:51:39 +0800

    添加免登

--- packages/pc/src/utils/publicUtil/login/index.ts
+++ packages/pc/src/utils/publicUtil/login/index.ts
@@ -15,16 +15,26 @@ class Login {
 
   protected checkSystemInfo() {
     // 系统服务信息 用于切换智信 或平台登录信息
-    const { userCode, token } = this.config.queryInfo;
+    const { userCode, token, appType } = this.config.queryInfo;
     // 智信环境
-    if (userCode) {
+    if (appType === "zhixin") {
       // 智信微应用环境
       this.config.env = "zx-env";
     }
 
     // 平台微应用环境
-    if (token) {
+    if (appType === "factory") {
       this.config.env = "mtip-app-env";
+    }
+
+    // 单机版微应用环境
+    if (appType === "single") {
+      this.config.env = "tpf-app-env";
+    }
+
+    // 第三方微应用环境
+    if (appType === "third") {
+      this.config.env = "other-app-env";
     }
 
     this.systemServerInfo = this.config.env;
@@ -50,6 +60,7 @@ class Login {
     if (url.indexOf("?") != -1) {
       let obj = {};
       let arr = url.slice(url.indexOf("?") + 1).split("&");
+      console.log(arr);
       arr.forEach((item) => {
         let param = item.split("=");
         obj[param[0]] = param[1];
@@ -106,11 +117,10 @@ class Login {
       return Promise.resolve(res);
       //    router.push("/");
     } else {
-      const tokenNew = token;
-      if (!appType) {
+      window.sessionStorage.setItem("token", token);
+      if (appType === "single") {
         const headers = {
           appType: "mtip-base-system",
-          token: tokenNew,
         };
         const res = (await this.config.axios.post(this.config.api, data, {
           headers,
